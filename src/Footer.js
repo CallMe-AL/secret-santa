9a57333c63a5e11abedd2ea1ddbfb0eb84c585ff
alex indovina <aindovina2@yahoo.com>
2022-11-30 22:38:07 -0500

    minor style changes, changed px to rem

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>No e-mail or personal info but a name required. :)</p>
-      <p>Happy Ho-Ho-Holidays! (lololol). By Alex</p>
+      <p>Happy Ho-Ho-Holidays! (lololol). By <a href='https://callme-al.github.io/portfolio/'>Alex</a> ❤️</p>
     </footer>
   )
 }
